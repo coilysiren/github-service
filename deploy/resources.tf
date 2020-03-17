45450c2c0c669de2519db09cf73e413ccaa172ca
Lynn <firemagelynn@gmail.com>
2020-03-17 00:26:00 -0700

    cleanup example

--- deploy/resources.tf
+++ deploy/resources.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "worker_group_mgmt_one" {
-  name_prefix = "worker_group_mgmt_one"
+resource "aws_security_group" "worker_management" {
+  name_prefix = "worker_management"
   vpc_id      = module.vpc.vpc_id
 
   ingress {
@@ -8,52 +8,19 @@ resource "aws_security_group" "worker_group_mgmt_one" {
     protocol  = "tcp"
 
     cidr_blocks = [
-      "10.0.0.0/8",
-    ]
-  }
-}
-
-resource "aws_security_group" "worker_group_mgmt_two" {
-  name_prefix = "worker_group_mgmt_two"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "192.168.0.0/16",
-    ]
-  }
-}
-
-resource "aws_security_group" "all_worker_mgmt" {
-  name_prefix = "all_worker_management"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-      "172.16.0.0/12",
-      "192.168.0.0/16",
+      "TODO",
     ]
   }
 }
 
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.6.0"
+  source = "terraform-aws-modules/vpc/aws"
 
   name                 = "test-vpc"
-  cidr                 = "10.0.0.0/16"
+  cidr                 = "TODO"
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets      = ["TODO"]
+  public_subnets       = ["TODO"]
   enable_nat_gateway   = true
   single_nat_gateway   = true
   enable_dns_hostnames = true
@@ -84,14 +51,13 @@ module "eks" {
 
   worker_groups = [
     {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
+      name                 = "worker-group-2"
+      instance_type        = "t2.medium"
+      asg_desired_capacity = 1
     },
   ]
 
-  worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  worker_additional_security_group_ids = [aws_security_group.worker_management.id]
   map_accounts = [
     var.AWS_ACCOUNT_ID,
   ]

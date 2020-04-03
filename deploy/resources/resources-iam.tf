1483e34390ef1b8d9fbd22f11d8ab5f7f395af9d
Lynn <firemagelynn@gmail.com>
2020-04-03 06:45:47 -0700

    docs

--- deploy/resources/resources-iam.tf
+++ deploy/resources/resources-iam.tf
@@ -1,6 +1,7 @@
 # purpose: the eks cluster runs as this role
 #
 # terraform docs: https://www.terraform.io/docs/providers/aws/d/iam_role.html
+# aws docs: https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles.html
 resource "aws_iam_role" "role" {
   name        = var.NAME
   description = "the eks cluster runs as this role"
@@ -22,17 +23,19 @@ resource "aws_iam_role" "role" {
 POLICY
 }
 
-# purpose: attach builtin aws eks cluster policy to our locally created role
+# purpose: attach the aws managed eks cluster policy to our locally created role
 #
-# terraform docs:https://www.terraform.io/docs/providers/aws/r/iam_role_policy_attachment.html
+# terraform docs: https://www.terraform.io/docs/providers/aws/r/iam_role_policy_attachment.html
+# aws docs: https://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_managed-vs-inline.html
 resource "aws_iam_role_policy_attachment" "cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.role.name
 }
 
-# purpose: attach builtin aws eks service policy to our locally created role
+# purpose: attach the aws managed eks service policy to our locally created role
 #
-# terraform docs:https://www.terraform.io/docs/providers/aws/r/iam_role_policy_attachment.html
+# terraform docs: https://www.terraform.io/docs/providers/aws/r/iam_role_policy_attachment.html
+# aws docs: https://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_managed-vs-inline.html
 resource "aws_iam_role_policy_attachment" "service_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
   role       = aws_iam_role.role.name
